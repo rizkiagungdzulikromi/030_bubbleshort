893f987c1006b24d3d2bd314db82a8b91c433ff4
rizkiagungdzulikromi <riskiagung3005@gmail.com>
2024-03-08 16:37:54 +0700

    menambahkan void unput

--- 030_bubbleshort.cpp
+++ 030_bubbleshort.cpp
@@ -6,3 +6,24 @@ int arr[20];           //Deklarasi array a dengan ukuran 20
 int n;               //Deklarasi variable n untuk menyimpan banyaknya elemen pada array
 
 
+void input() {       //Method untuk input
+    while (true) {   //Looping
+        cout << "Masukan array banyaknya elemen pada array : "; //Output ke layar
+        cin >> n;    //Input dari pengguna
+        if (n <= 20) //Jika n kurang dari atau sama dengan 20
+            break;   //Keluar dari loop
+        else {       //Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; //Output ke layar
+        }
+    }
+    cout << endl;                            //Output baris kosong
+    cout << "====================" << endl;  //Output ke layar
+    cout << "Masukan Elemen Array" << endl;  //Output ke layar
+    cout << "====================" << endl;  //Output ke layar
+
+    for (int i = 0; i < n; i++) {     //Looping ddengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": "; //Output ke layar
+        cin >> arr[i];                  //Input dari pengguna
+    }
+}
+
